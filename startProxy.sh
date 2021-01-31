627e01dd106dd07dc03e8899380e9301d9aa66b6
aboyce <adam@aboyce.uk>
2021-01-31 10:54:54 +0000

    Added URL output to script

--- startProxy.sh
+++ startProxy.sh
@@ -1,2 +1,5 @@
+echo "Dashboard is available at:"
+echo "http://$(hostname):8001/api/v1/namespaces/kube-system/services/https:kubernetes-dashboard:/proxy"
+echo ""
 microk8s kubectl proxy --address='0.0.0.0' --accept-hosts='^*$'
 
