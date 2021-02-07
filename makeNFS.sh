6e2394c194b92ad2f14711f7ae3e615bcf80c493
aboyce <adam@aboyce.uk>
2021-02-07 19:14:37 +0000

    Moved NFS creation details.

---
+++ makeNFS.sh
@@ -0,0 +1,15 @@
+# Host - Update the servers who can interact
+sudo nano /etc/exports
+
+# Host - Restart NFS for changes to apply
+sudo systemctl restart nfs-kernel-server
+
+# Client - Mount the share
+sudo mkdir -p /nfs
+sudo mount [HOST_IP]:/var/nfs /nfs
+
+# Client - Check it is mounted
+dh -h
+
+# Client - Unmount (move out of the shared directory)
+sudo umount /nfs
